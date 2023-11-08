888043fc95293b9398df6f04d2ede442650991bd
Ainun_Rofiq H <ainunrofiq7@gmail.com>
2023-11-08 12:27:39 +0700

    first commit

---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function HomePage(){
+    return(
+        <Fragment>
+            <div className="">
+                <h1>Hello Word</h1>
+            </div>
+        </Fragment>
+    )
+}
+export default HomePage
